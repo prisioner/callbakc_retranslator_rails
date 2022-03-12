af3bc44aa218652bc93d620170b905678c2a56cb
Aleksandr Klimenkov <prisioner@mail.ru>
2022-03-12 08:38:46 +0300

    update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  post "/bot#{ENV['BOT_TOKEN'].tr(':', '_')}", to: 'bot#command'
+  post "/bot#{ENV['BOT_TOKEN']}", to: 'bot#command'
 
   post '/p/:chat_id/:username/(:message_source)', to: 'bot#callback'
 end
